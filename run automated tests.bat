80d2220d27ca0655ae9bd34295b8667567e5387d
Arbi <arbij2002@gmail.com>
2026-05-14 06:38:12 +0200

    small fix

--- run automated tests.bat
+++ run automated tests.bat
@@ -1,15 +1,15 @@
 @echo off
 
-pushd "test database/reset"
-call run.bat
-popd
-
 pushd server
 start cmd /c "run test.bat"
 popd
 
 pushd "payment provider server"
 start cmd /c run.bat
+popd
+
+pushd "test database/reset"
+call run.bat
 popd
 
 cd tests
